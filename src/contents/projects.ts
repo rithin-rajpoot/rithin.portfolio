5869c903e2ead420e43d96cc59e3c4c555db7c60
rithin-rajpoot <rithinrajpoot@gmail.com>
2025-09-28 18:59:42 +0530

    Added new project, chronicalX - A blog platform

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -1,45 +1,84 @@
-
 import { Project } from "@/types";
 
 export const projects: Project[] = [
-   {
-        title: 'RakthSetu',
-        description: 'A MERN-based platform for real-time donor-seeker interaction, facilitating blood donation and emergency requests.',
-        technologies: ['React.js', 'Redux toolkit', 'Socket.io', 'MongoDB', 'Node.js', 'Express.js', 'Tailwind CSS', 'Google Maps API'],
-        githubLink: 'https://github.com/rithin-rajpoot/rakthsetu',
-        demoLink: 'https://rakthsetu-v1.vercel.app/',
-        image: '/projects/rakthsetu.png',
-      },
-      {
-        title: 'EliteShop',
-        description: 'EliteShop is a modern, responsive E-commerce Frontend featuring a clean UI, product catalog, shopping cart, checkout, authentication, and order history, all managed on the client side',
-        technologies: ['React.js', 'Context API', 'Tailwind CSS'],
-        githubLink: 'https://github.com/rithin-rajpoot/elite-shop',
-        demoLink: 'https://elite-shop-mu.vercel.app/',
-        image: '/projects/elite-shop.png',
-      },{
-        title: 'Portfolio Website',
-        description: 'My personal Portfolio Website — Built using Next.js and Framer Motion, this personal portfolio showcases modern transitions, smooth animations, and a clean, responsive layout. Designed to highlight projects and skills with an engaging user experience.',
-        technologies: ['Next.js', 'TypeScript', 'Framer Motion', 'Tailwind CSS'],
-        githubLink: 'https://github.com/rithin-rajpoot/rithin.portfolio',
-        demoLink: 'https://rithin-portfolio.vercel.app/',
-        image: '/projects/portfolio-website.png',
-      },
-      {
-        title: 'Gup Shup',
-        description: 'Gup Shup is a real-time chat application built with the MERN stack and Redux Toolkit. It features secure authentication (JWT), real-time messaging (Socket.io), a fully responsive design using Tailwind CSS, and over 32 customizable UI themes to enhance user experience.',
-        technologies: ['React', 'Node.js', 'MongoDB', 'Socket.io', 'Redux Toolkit', 'Tailwind CSS'],
-        githubLink: 'https://github.com/rithin-rajpoot/CHAT-APP',
-        demoLink: 'https://chat-app-bhh8.vercel.app',
-        image: '/projects/chat-app.png',
-      },
-      {
-        title: 'Complaint Management System',
-        description: 'A modern, efficient complaint management system built with cutting-edge web technologies.',
-        technologies: ['MySQL', 'Express.js', 'EJS', 'Tailwind CSS'],
-        githubLink: 'https://github.com/rithin-rajpoot/complaint-management-system',
-        demoLink: 'https://github.com/rithin-rajpoot/complaint-management-system',
-        image: '/projects/CMS.png',
-      },
-      
-  ];
+  {
+    title: "ChronicalX",
+    description:
+      "A full-stack blog platform. Features user authentication, post management, comments, likes, and modern UI design.",
+    technologies: [
+      "React.js",
+      "Redux Toolkit",
+      "JWT",
+      "Cloudinary",
+      "OAuth",
+      "MongoDB",
+      "Node.js",
+      "Express.js",
+      "Tailwind CSS"
+    ],
+    githubLink: "https://github.com/rithin-rajpoot/chronical-x",
+    demoLink: "https://chronical-x.vercel.app/",
+    image: "/projects/chronical-x.png"
+  },
+  {
+    title: "RakthSetu",
+    description:
+      "A MERN-based platform for real-time donor-seeker interaction, facilitating blood donation and emergency requests.",
+    technologies: [
+      "React.js",
+      "Redux toolkit",
+      "Socket.io",
+      "MongoDB",
+      "Node.js",
+      "Express.js",
+      "Tailwind CSS",
+      "Google Maps API",
+    ],
+    githubLink: "https://github.com/rithin-rajpoot/rakthsetu",
+    demoLink: "https://rakthsetu-v1.vercel.app/",
+    image: "/projects/rakthsetu.png",
+  },
+  {
+    title: "EliteShop",
+    description:
+      "EliteShop is a modern, responsive E-commerce Frontend featuring a clean UI, product catalog, shopping cart, checkout, authentication, and order history, all managed on the client side",
+    technologies: ["React.js", "Context API", "Tailwind CSS"],
+    githubLink: "https://github.com/rithin-rajpoot/elite-shop",
+    demoLink: "https://elite-shop-mu.vercel.app/",
+    image: "/projects/elite-shop.png",
+  },
+  {
+    title: "Portfolio Website",
+    description:
+      "My personal Portfolio Website — Built using Next.js and Framer Motion, this personal portfolio showcases modern transitions, smooth animations, and a clean, responsive layout. Designed to highlight projects and skills with an engaging user experience.",
+    technologies: ["Next.js", "TypeScript", "Framer Motion", "Tailwind CSS"],
+    githubLink: "https://github.com/rithin-rajpoot/rithin.portfolio",
+    demoLink: "https://rithin-portfolio.vercel.app/",
+    image: "/projects/portfolio-website.png",
+  },
+  {
+    title: "Gup Shup",
+    description:
+      "Gup Shup is a real-time chat application built with the MERN stack and Redux Toolkit. It features secure authentication (JWT), real-time messaging (Socket.io), a fully responsive design using Tailwind CSS, and over 32 customizable UI themes to enhance user experience.",
+    technologies: [
+      "React",
+      "Node.js",
+      "MongoDB",
+      "Socket.io",
+      "Redux Toolkit",
+      "Tailwind CSS",
+    ],
+    githubLink: "https://github.com/rithin-rajpoot/CHAT-APP",
+    demoLink: "https://chat-app-bhh8.vercel.app",
+    image: "/projects/chat-app.png",
+  },
+  {
+    title: "Complaint Management System",
+    description:
+      "A modern, efficient complaint management system built with cutting-edge web technologies.",
+    technologies: ["MySQL", "Express.js", "EJS", "Tailwind CSS"],
+    githubLink: "https://github.com/rithin-rajpoot/complaint-management-system",
+    demoLink: "https://github.com/rithin-rajpoot/complaint-management-system",
+    image: "/projects/CMS.png",
+  },
+];
